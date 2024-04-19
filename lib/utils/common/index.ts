db120fd391b3743e758292bebb0be15214bcccd9
huadee1108 <981929654@qq.com>
2024-04-19 23:59:01 +0800

    feat: test

--- lib/utils/common/index.ts
+++ lib/utils/common/index.ts
@@ -1,10 +1,10 @@
-import getValueByPath from "./getValueByPath";
-import replaceVariables from "./replaceVariables";
-import transferObjToList from "./transferObjToList";
-import functionParser from "./functionParser";
-import isNormalObject from "./isNormalObject";
-import getParentValueAndKeyByPath from "./getParentValueAndKeyByPath";
-import getUuid from "./getUuid";
+import getValueByPath from "./getValueByPath.js";
+import replaceVariables from "./replaceVariables.js";
+import transferObjToList from "./transferObjToList.js";
+import functionParser from "./functionParser.js";
+import isNormalObject from "./isNormalObject.js";
+import getParentValueAndKeyByPath from "./getParentValueAndKeyByPath.js";
+import getUuid from "./getUuid.js";
 
 export {
   getValueByPath,
